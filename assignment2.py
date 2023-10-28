35e61e0340b7aad7c6bc0532a8ed5cc2ff94779e
Niiazbekbb <144800349+Niiazbekbb@users.noreply.github.com>
2023-10-28 14:34:46 +0900

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,10 +1,8 @@
 list_words = []
 with open('romeo.txt', 'r') as file:
     for line in file:
-        words = line.split()
-        for word in words:
-            if word not in list_words:
-                list_words.append(word)
-list_words.sort()
+        list_words.extend(line.split())
+list_words = sorted(set(list_words))
 for word in list_words:
     print(word)
+
